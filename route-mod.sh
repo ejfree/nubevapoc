a2da591114edd46f32d8fd579a55b9041c001f50
Erik Freeland <erik.freeland@gmail.com>
2017-12-29 12:02:33 -0800

    Delete route-mod.sh

--- route-mod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#Modify routing on peer to support Internet connectivity
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo ip route add 10.10.0.0/16 via 10.11.1.1
