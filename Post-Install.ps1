2e14985c789433545996eabd31fcfbcef848e53b
Erik Freeland <erik.freeland@gmail.com>
2018-01-12 14:39:24 -0800

    Create Post-Install.ps1

---
+++ Post-Install.ps1
@@ -0,0 +1,3 @@
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+Invoke-WebRequest https://dl.pstmn.io/download/latest/win64 -outfile .\postman.exe
+.\postman.exe /s
