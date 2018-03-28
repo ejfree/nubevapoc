1afa9995eea86f8af9d3d1c1c30f0040a78e3f94
Erik Freeland <erik.freeland@gmail.com>
2018-03-28 09:30:32 -0700

    Added installation of vswitch-common

--- add_vxlan.sh
+++ add_vxlan.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-#Add OVS and vxlan interface to bastion. 
+#Add OVS and vxlan interface to bastion.
+sudo apt-get update -y
+sudo apt-get install openvswitch-common -y
 sudo apt-get install openvswitch-switch -y
 sudo ovs-vsctl add-br br2
 sudo ovs-vsctl add-port br2 vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=10.10.200.10
